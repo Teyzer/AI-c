4ec9bb44834a343159d22bb5b3f76208e816c4a5
Teyzer <45541358+Teyzer@users.noreply.github.com>
2023-01-29 13:55:53 +0100

    Proper start

--- main.c
+++ main.c
@@ -5,43 +5,12 @@
 #include "utils.h"
 #include "toolbox.h"
 
-// void f(intc n) {
-//     for (int i = 0; i < n; i++) {
-        
-//     }
-// }
-
-doublec h(intc a, intc b) {
-    return (b + 3 * a + 1);
-}
-
-doublec g(intc a, intc b) {
-    return a + b + 1;
-}
-
 int main() {
 
     // Laisse toujours cette fonction
     Initialize();
 
-    FILE *fp=NULL;
-    fp=fopen("circle.txt","w");
-    double r;
-    double x,y,x0,y0;
-    printf("Enter the radius of the circle to be plotted: ");
-    scanf("%lf",&r);
-    printf("Enter the x and y-coordinates of the center: ");
-    scanf("%lf%lf",&x0,&y0);
-    for(y=y0-r;y<=y0+r;y=y+0.1){
-        x=sqrt(r*r-(y-y0)*(y-y0))+x0; 
-        fprintf(fp,"%lf\t %lf\n",x,y);
-    }
-    for(y=y0+r;y>=y0-r;y=y-0.1){
-        x=-sqrt(r*r-(y-y0)*(y-y0))+x0; 
-        fprintf(fp,"%lf\t %lf\n",x,y);
-    }
-    
-
+    print("Beginning");    
 
     // Laisse là toujours aussi
     Finish();
